0acd3f4818a955c98140f7bce6b62ae3bcd8b869
yukselsule <sulecantekin@gmail.com>
2024-02-14 19:36:39 +0300

    done

--- script.js
+++ script.js
@@ -10,12 +10,38 @@ const newGame = document.querySelector(".btn");
 // Variables
 
 let match = 0;
-let move = 0;
+let move = 10;
 let limit = 0;
 let openCards = [];
 let flippedCards = [];
 
 // Functions
+
+// starting conditions - new game
+const init = function () {
+  match = 0;
+  move = 10;
+
+  matches.textContent = 0;
+  moves.textContent = 10;
+
+  document.querySelector(".winner").style.display = "none";
+  document.querySelector(".cards").style.display = "grid";
+
+  const flippedCards = document.querySelectorAll(".flipped");
+  flippedCards.forEach((flippedCard) => {
+    flippedCard.classList.remove("flipped");
+  });
+
+  const openCards = document.querySelectorAll(".matched");
+  openCards.forEach((openCard) => {
+    openCard.classList.remove("matched");
+  });
+
+  setTimeout(() => {
+    shuffle();
+  }, 500);
+};
 
 // shuffling the cards
 const shuffle = function () {
@@ -27,15 +53,63 @@ const shuffle = function () {
 };
 
 // game over
-const gameOver = function () {
+const gameWon = function () {
   setTimeout(() => {
     document.querySelector(".winner").style.display = "inherit";
     document.querySelector(".cards").style.display = "none";
   }, 1500);
 };
 
-// open card
+const gameLost = function () {
+  setTimeout(() => {
+    document.querySelector(".loser").style.display = "inherit";
+    document.querySelector(".cards").style.display = "none";
+  }, 1500);
+};
 
+// cards matched
+const cardsMatched = function (flippedCards) {
+  match++;
+  matches.textContent = match;
+  const isGameWon = match === 8;
+
+  flippedCards.forEach((flippedCard) => {
+    flippedCard.classList.add("matched");
+    flippedCard.classList.remove("flipped");
+
+    if (isGameWon) {
+      gameWon();
+    }
+  });
+};
+
+// cards not matched
+const closeCards = function (flippedCards) {
+  flippedCards.forEach((flippedCard) => {
+    setTimeout(() => {
+      flippedCard.classList.remove("flipped");
+    }, 1000);
+  });
+  move--;
+  moves.textContent = move;
+  const isGameLost = move === 0;
+
+  if (isGameLost) {
+    gameLost();
+  }
+};
+
+init();
+
+// Flipping card
+
+cards.forEach((card) => {
+  card.addEventListener("click", function () {
+    openCard(card);
+  });
+});
+
+// open card
 const openCard = function (card) {
   if (
     !card.classList.contains("flipped") &&
@@ -59,75 +133,9 @@ const openCard = function (card) {
 
       openCards = [];
       flippedCards = [];
-
-      move++;
-      moves.textContent = move;
     }
   }
 };
-
-// cards matched
-
-const cardsMatched = function (flippedCards) {
-  match++;
-  matches.textContent = match;
-  const isGameOver = match === 8;
-
-  flippedCards.forEach((flippedCard) => {
-    flippedCard.classList.add("matched");
-    flippedCard.classList.remove("flipped");
-
-    if (isGameOver) {
-      gameOver();
-    }
-  });
-};
-
-// cards not matched
-
-const closeCards = function (flippedCards) {
-  flippedCards.forEach((flippedCard) => {
-    setTimeout(() => {
-      flippedCard.classList.remove("flipped");
-    }, 1000);
-  });
-};
-
-// starting conditions - new game
-const init = function () {
-  match = 0;
-  move = 0;
-
-  matches.textContent = 0;
-  moves.textContent = 0;
-
-  document.querySelector(".winner").style.display = "none";
-  document.querySelector(".cards").style.display = "grid";
-
-  const flippedCards = document.querySelectorAll(".flipped");
-  flippedCards.forEach((flippedCard) => {
-    flippedCard.classList.remove("flipped");
-  });
-
-  const openCards = document.querySelectorAll(".matched");
-  openCards.forEach((openCard) => {
-    openCard.classList.remove("matched");
-  });
-
-  setTimeout(() => {
-    shuffle();
-  }, 500);
-};
-
-init();
-
-// Flipping card
-
-cards.forEach((card) => {
-  card.addEventListener("click", function () {
-    openCard(card);
-  });
-});
 
 // new game button
 
